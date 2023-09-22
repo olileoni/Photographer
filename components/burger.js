b25cd42f4ff8cbea2053b9897c12eaecb9211f0d
oli.leoni <oli.leud18@gmail.com>
2023-09-22 18:44:56 +0300

    fixing

--- components/burger.js
+++ components/burger.js
@@ -1,6 +1,8 @@
 const burgerBtn = document.querySelector('.header__burger');
 const header = document.querySelector('.header');
+const body = document.body
 
 burgerBtn.addEventListener('click', () => {
     header.classList.toggle('open')
+    body.classList.toggle('lock')
 })
